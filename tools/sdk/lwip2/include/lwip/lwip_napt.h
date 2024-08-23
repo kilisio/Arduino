53012e0e8a92f62aed3573eacde76a3c0c692131
kilisio <pkilisio@gmail.com>
2024-08-23 13:33:28 +0300

    remove napt patches

--- tools/sdk/lwip2/include/lwip/lwip_napt.h
+++ tools/sdk/lwip2/include/lwip/lwip_napt.h
@@ -1,10 +1,12 @@
 /**
- * @file
- * Network Address and Port Translation
+ * @file lwip_napt.h
+ * public API of ip4_napt
+ *
+ * @see ip4_napt.c
  */
 
 /*
- * Copyright (c) 2016-2019 NeoCat <neocat@neocat.jp>
+ * Copyright (c) 2001-2004 Swedish Institute of Computer Science.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -31,7 +33,7 @@
  *
  * This file is part of the lwIP TCP/IP stack.
  *
- * Author: NeoCat <neocat@neocat.jp>
+ * original reassembly code by Adam Dunkels <adam@sics.se>
  *
  */
 
@@ -39,7 +41,7 @@
 #define __LWIP_NAPT_H__
 
 #include "lwip/opt.h"
-#include "lwip/err.h"
+#include "lwip/netif.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -49,54 +51,32 @@ extern "C" {
 #if IP_NAPT
 
 /* Default size of the tables used for NAPT */
+#ifndef IP_NAPT_MAX
 #define IP_NAPT_MAX 512
+#endif
+#ifndef IP_PORTMAP_MAX
 #define IP_PORTMAP_MAX 32
+#endif
 
 /* Timeouts in sec for the various protocol types */
-#define IP_NAPT_TIMEOUT_MS_TCP (30*60*1000)
-#define IP_NAPT_TIMEOUT_MS_TCP_DISCON (20*1000)
+#ifndef IP_NAPT_TIMEOUT_MS_TCP
+#define IP_NAPT_TIMEOUT_MS_TCP (5*60*1000)
+#endif
+#ifndef IP_NAPT_TIMEOUT_MS_TCP_DISCON
+#define IP_NAPT_TIMEOUT_MS_TCP_DISCON (TCP_MSL)
+#endif
+#ifndef IP_NAPT_TIMEOUT_MS_UDP
 #define IP_NAPT_TIMEOUT_MS_UDP (2*1000)
+#endif
+#ifndef IP_NAPT_TIMEOUT_MS_ICMP
 #define IP_NAPT_TIMEOUT_MS_ICMP (2*1000)
-
+#endif
+#ifndef IP_NAPT_PORT_RANGE_START
 #define IP_NAPT_PORT_RANGE_START 49152
+#endif
+#ifndef IP_NAPT_PORT_RANGE_END
 #define IP_NAPT_PORT_RANGE_END   61439
-
-struct napt_table {
-  u32_t last;
-  u32_t src;
-  u32_t dest;
-  u16_t sport;
-  u16_t dport;
-  u16_t mport;
-  u8_t proto;
-  u8_t fin1 : 1;
-  u8_t fin2 : 1;
-  u8_t finack1 : 1;
-  u8_t finack2 : 1;
-  u8_t synack : 1;
-  u8_t rst : 1;
-  u16_t next, prev;
-};
-
-struct portmap_table {
-  u32_t maddr;
-  u32_t daddr;
-  u16_t mport;
-  u16_t dport;
-  u8_t proto;
-  u8_t valid;
-};
-
-extern struct portmap_table *ip_portmap_table;
-
-/**
- * Allocates and initializes the NAPT tables.
- *
- * @param max_nat max number of enties in the NAPT table (use IP_NAPT_MAX if in doubt)
- * @param max_portmap max number of enties in the NAPT table (use IP_PORTMAP_MAX if in doubt)
- */
-err_t
-ip_napt_init(u16_t max_nat, u8_t max_portmap);
+#endif
 
 
 /**
@@ -105,18 +85,31 @@ ip_napt_init(u16_t max_nat, u8_t max_portmap);
  * @param addr ip address of the interface
  * @param enable non-zero to enable NAPT, or 0 to disable.
  */
-err_t
+void
 ip_napt_enable(u32_t addr, int enable);
 
 
 /**
  * Enable/Disable NAPT for a specified interface.
  *
- * @param netif number of the interface
+ * @param number number of the interface
  * @param enable non-zero to enable NAPT, or 0 to disable.
  */
-err_t
+void
 ip_napt_enable_no(u8_t number, int enable);
+
+/**
+ * Enable/Disable NAPT for a specified interface.
+ *
+ * @param netif interface handle
+ * @param enable non-zero to enable NAPT, or 0 to disable.
+ *
+ * @return
+ *         - 0: Failure
+ *         - 1: Success
+ */
+int
+ip_napt_enable_netif(struct netif *netif, int enable);
 
 
 /**
@@ -130,18 +123,33 @@ ip_napt_enable_no(u8_t number, int enable);
  * @param daddr destination ip address
  * @param dport destination port, in host byte order.
  */
-u8_t 
+u8_t
 ip_portmap_add(u8_t proto, u32_t maddr, u16_t mport, u32_t daddr, u16_t dport);
+
+u8_t
+ip_portmap_get(u8_t proto, u16_t mport, u32_t *maddr, u32_t *daddr, u16_t *dport);
 
 
 /**
  * Unregister port mapping on the external interface to internal interface.
  *
  * @param proto target protocol
- * @param maddr ip address of the external interface
+ * @param mport mapped port on the external interface, in host byte order.
  */
-u8_t 
+u8_t
 ip_portmap_remove(u8_t proto, u16_t mport);
+
+
+
+#if LWIP_STATS
+/**
+ * Get statistics.
+ *
+ * @param stats struct to receive current stats
+ */
+void
+ip_napt_get_stats(struct stats_ip_napt *stats);
+#endif
 
 #endif /* IP_NAPT */
 #endif /* IP_FORWARD */
