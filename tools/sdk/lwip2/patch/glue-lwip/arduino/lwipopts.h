741830511dab4d460495fc71d1f34af915f1f31c
kilisio <pkilisio@gmail.com>
2024-09-19 09:47:13 +0300

    revert maximum segment lifetime to original value

--- tools/sdk/lwip2/patch/glue-lwip/arduino/lwipopts.h
+++ tools/sdk/lwip2/patch/glue-lwip/arduino/lwipopts.h
@@ -128,12 +128,6 @@
 #undef LWIP_DHCP
 #define LWIP_DHCP                       1
 
-#undef TCP_MSL  
-#define TCP_MSL 100UL /* The maximum segment lifetime in milliseconds */
-
-#undef TCP_TMR_INTERVAL
-#define TCP_TMR_INTERVAL                250  /* The TCP timer interval in milliseconds. */
-
 #undef LWIP_CHECKSUM_ON_COPY           
 #define LWIP_CHECKSUM_ON_COPY           1
 
